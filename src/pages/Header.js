d115a817130264f02941d47d81b6221f492db293
eessalmi <el-mustapha.es-salmi@cagemini.com>
2023-08-07 18:46:13 +0100

    initial push

---
+++ src/pages/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+// const Hello = () => {
+//   return (
+//     <div>
+//       <h1>Hello, World!</h1>
+//       <p>This is my first React component.</p>
+//     </div>
+//   );
+// }
+
+
+export default function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is my first React component.</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+
